2449d5de9d4c89d616afb01125b46dc96e1934c9
Zahir <github@a20.co.za>
2019-09-28 21:20:41 +0200

    script fixed

--- virtualenv_cmd.sh
+++ virtualenv_cmd.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 VIRTUAL_ENV=$1
 WORKING_DIRECTORY=$2
 if [ -z $VIRTUAL_ENV ] || [ -z $WORKING_DIRECTORY ]; then
@@ -7,8 +6,8 @@ if [ -z $VIRTUAL_ENV ] || [ -z $WORKING_DIRECTORY ]; then
   exit 1
 fi
 echo "activating virtual env with $VIRTUAL_ENV/bin/activate"
-#. $VIRTUAL_ENV/bin/activate
-#echo "activated"
+. $VIRTUAL_ENV/bin/activate
+echo "activated"
 pushd $WORKING_DIRECTORY
 echo "pushd $WORKING_DIRECTORY"
 shift 2
