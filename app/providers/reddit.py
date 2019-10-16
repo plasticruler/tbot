783b5c2780321423aeecb234cf4a1398062ec469
Zahir <github@a20.co.za>
2019-10-16 20:47:18 +0200

    update caption text, remove send email on exception

--- app/providers/reddit.py
+++ app/providers/reddit.py
@@ -1,9 +1,17 @@
 from . import BaseProvider
+from app import db
+from app.main.models import ContentItem, ContentTag
+
+
 class RedditContentProvider(BaseProvider):
     def __init__(self, name):
         super(name)
         print(self.providerName)
-    pass                                                     
-    def execute(self, **kwargs):
-        
+    pass
+
+    def getRandomItem(self, **kwargs):
+        super(**kwargs)
+        pass
+    
+    def execute(self, **kwargs):        
         pass
