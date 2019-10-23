aa5b78ddb98b2f2b3cd9384ac0094810f08d9dd1
Zahir <github@a20.co.za>
2019-10-23 07:26:50 +0200

    start scripts + new provider XKCD + send message to chatid via command line

--- run_website.sh
+++ run_website.sh
@@ -3,6 +3,6 @@ source $HOME/dev/github/tbot/.venv/bin/activate
 pushd $HOME/dev/github/tbot
 export FLASK_APP=tbot.py
 export FLASK_DEBUG=1
-python $HOME/dev/github/tbot/distractobot.py
+flask run --port=5000 --host=nest
 popd
 deactivate
